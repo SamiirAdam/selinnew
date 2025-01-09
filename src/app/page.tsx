8c3fb33b7cb2f2efdafa3874a5470b182a6b775b
Adam <coding@samiir.nl>
2025-01-09 19:51:59 +0100

    sd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,38 @@
+"use client";
+
 import Header from './../components/Header'
 import Hero from './../components/Hero'
 import Projects from './../components/Projects'
 import Skills from './../components/Skills'
 import Contact from './../components/Contact'
+import { useState, useEffect } from 'react'
+import Particle from '@/components/Particles';
+import { withClick } from '@/components/Pre';
 
 export default function PortfolioPage() {
+  const [load, upadateLoad] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const ClickableCard = withClick(Hero, Hero);
+
   return (
-    <div className="flex flex-col max-h-screen w-full text-foreground">
-      <Header />
-      <main>
-        <Hero />
-        <Projects />
-        <Skills />
-        <Contact />
-      </main>
+    <div>
+      <Particle />
+      <div className="flex flex-col max-h-screen w-full text-foreground relative">
+        <Header />
+        <main>
+          <ClickableCard />
+          <Projects />
+          <Skills />
+          <Contact />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Pre.tsx
@@ -0,0 +1,128 @@
+import type { ComponentType } from "react"
+import { motion, useSpring } from "framer-motion"
+import React, { useState, useRef, useEffect } from "react"
+
+const spring = {
+    type: "spring",
+    stiffness: 300,
+    damping: 40,
+}
+
+export function withClick<T extends object>(
+    ComponentFront: ComponentType<T>,
+    ComponentBack: ComponentType<T>,
+): ComponentType<T> {
+    return (props) => {
+        const [isFlipped, setIsFlipped] = useState(false);
+
+        const handleClick = () => {
+            setIsFlipped((prevState) => !prevState);
+        };
+
+        const [rotateXaxis, setRotateXaxis] = useState(0);
+        const [rotateYaxis, setRotateYaxis] = useState(0);
+        const ref = useRef<HTMLDivElement>(null);
+
+        const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+            const element = ref.current;
+            if (!element) return;
+            const elementRect = element.getBoundingClientRect();
+            const elementWidth = elementRect.width;
+            const elementHeight = elementRect.height;
+            const elementCenterX = elementWidth / 2;
+            const elementCenterY = elementHeight / 2;
+            const mouseX = event.clientY - elementRect.y - elementCenterY;
+            const mouseY = event.clientX - elementRect.x - elementCenterX;
+            setRotateXaxis(mouseX / elementHeight * 30);
+            setRotateYaxis(mouseY / elementWidth * 30);
+        };
+
+        const handleMouseEnd = () => {
+            setRotateXaxis(0)
+            setRotateYaxis(0)
+        }
+
+        const dx = useSpring(0, spring)
+        const dy = useSpring(0, spring)
+
+        useEffect(() => {
+            dx.set(-rotateXaxis)
+            dy.set(rotateYaxis)
+        }, [rotateXaxis, rotateYaxis])
+
+        return (
+            <motion.div
+                onClick={handleClick}
+                transition={spring}
+                className="w-full h-full"
+                style={{
+                    perspective: "2000px",
+                    transformStyle: "preserve-3d",
+                }}
+            >
+                <motion.div
+                    ref={ref}
+                    whileHover={{ scale: 1.07 }}
+                    onMouseMove={handleMouseMove}
+                    onMouseLeave={handleMouseEnd}
+                    transition={spring}
+                    className="w-full h-full"
+                    style={{
+                        rotateX: dx,
+                        rotateY: dy,
+                    }}
+                >
+                    <div
+                        style={{
+                            perspective: "2000px",
+                            transformStyle: "preserve-3d",
+                            width: "100%",
+                            height: "100%",
+                        }}
+                    >
+                        <motion.div
+                            animate={{ rotateY: isFlipped ? -180 : 0 }}
+                            transition={spring}
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                zIndex: isFlipped ? 0 : 1,
+                                backfaceVisibility: "hidden",
+                                position: "absolute",
+                            }}
+                        >
+                            <ComponentFront
+                                {...props}
+                                variant="Front"
+                                style={{
+                                    width: "100%",
+                                    height: "100%",
+                                }}
+                            />
+                        </motion.div>
+                        <motion.div
+                            initial={{ rotateY: 180 }}
+                            animate={{ rotateY: isFlipped ? 0 : 180 }}
+                            transition={spring}
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                zIndex: isFlipped ? 1 : 0,
+                                backfaceVisibility: "hidden",
+                            }}
+                        >
+                            <ComponentBack
+                                {...props}
+                                variant="Back"
+                                style={{
+                                    width: "100%",
+                                    height: "100%",
+                                }}
+                            />
+                        </motion.div>
+                    </div>
+                </motion.div>
+            </motion.div>
+        )
+    }
+}
